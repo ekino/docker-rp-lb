d6414ca35b80c432f60c1fb5b9136eb6355fefa6
Matthieu Fronton <fronton@ekino.com>
2016-03-10 16:37:39 +0100

    Do not recreate certificate at each start

--- haproxy/haproxy.sh
+++ haproxy/haproxy.sh
@@ -2,14 +2,18 @@
 #set -x
 echo "$green ==> Starting ekino/haproxy:base$reset"
 
-echo "$cyan --> Setting ssl certificate$reset"
-
 SSL_DEFAULT="/etc/ssl/private/haproxy.pem"
 if [ -z ${SSL_CERT} ] || [ ! -f $SSL_DEFAULT ]
 then
   SSL_CERT="$SSL_DEFAULT"
 fi
 
-mkdir -pv "$(dirname $SSL_CERT)"
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$SSL_CERT" -out "$SSL_CERT" -batch
+if [ ! -f "$SSL_CERT" ]
+then
+  echo "$cyan --> Setting SSL certificate$reset"
+  mkdir -pv "$(dirname $SSL_CERT)"
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$SSL_CERT" -out "$SSL_CERT" -batch
+else
+  echo "$cyan --> SSL certificate already setup$reset"
+fi
 
